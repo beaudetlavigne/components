1823cc53c6800e48eb100f6c4f62d2c78de3c3d6
beaudetlavigne <60715104+beaudetlavigne@users.noreply.github.com>
2025-06-24 15:59:58 -0400

    Create Navbar.jsx

---
+++ Navbar.jsx
@@ -0,0 +1,14 @@
+import { FormattedMessage } from "react-intl";
+export const Navbar = ({ locale, setLocale }) => (
+  <nav className="p-4 flex justify-between items-center bg-black">
+    <h1 className="text-xl font-bold">🎶 Composer</h1>
+    <select
+      className="bg-neutral-800 text-white p-1 rounded"
+      value={locale}
+      onChange={(e) => setLocale(e.target.value)}
+    >
+      <option value="fr">FR</option>
+      <option value="en">EN</option>
+    </select>
+  </nav>
+);

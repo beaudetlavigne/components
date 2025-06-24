2b11e101a345b02ac0e0056bdc5d160b7d39c448
beaudetlavigne <60715104+beaudetlavigne@users.noreply.github.com>
2025-06-24 16:10:31 -0400

    Update Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -1,3 +1,4 @@
+// components/Footer.jsx
 export const Footer = () => (
   <footer className="p-4 text-center text-sm opacity-50">
     &copy; {new Date().getFullYear()} Compositeur. Tous droits réservés.

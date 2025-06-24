e34f01c88730e8f857246e8dbdf179af0cf52607
beaudetlavigne <60715104+beaudetlavigne@users.noreply.github.com>
2025-06-24 16:56:53 -0400

    Create Contact.jsx

---
+++ Contact.jsx
@@ -0,0 +1,13 @@
+-- components/Contact.jsx
+export const Contact = () => (
+  <section className="p-6 text-center bg-neutral-800 rounded-2xl m-6">
+    <h3 className="text-2xl font-bold mb-2">📩 Contact</h3>
+    <p className="mb-2">Pour toute collaboration ou projet, contactez-moi :</p>
+    <a
+      href="mailto:ton.email@example.com"
+      className="text-blue-400 underline"
+    >
+      ton.email@example.com
+    </a>
+  </section>
+);
